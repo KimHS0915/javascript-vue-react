0f9daac481e55a81f19ec61be14009eb79b5bdeb
KimHS0915 <remember2552@gmail.com>
2020-03-25 21:54:22 +0900

    Add result

--- javascript/Minesweeper.js
+++ javascript/Minesweeper.js
@@ -1,8 +1,13 @@
 var tbody = document.querySelector('#table tbody');
 var dataset = [];
+var stopFlag = false;
+var openBox = 0;
 document.querySelector('#exec').addEventListener('click', function() {
   tbody.innerHTML = '';
-  dataset = []
+  document.querySelector('#result').textContent = '';
+  dataset = [];
+  openBox = 0;
+  stopFlag = false;
   var hor = parseInt(document.querySelector('#hor').value);
   var ver = parseInt(document.querySelector('#ver').value);
   var mine = parseInt(document.querySelector('#mine').value);
@@ -27,6 +32,9 @@ document.querySelector('#exec').addEventListener('click', function() {
       var td = document.createElement('td');
       td.addEventListener('contextmenu', function (e) {
         e.preventDefault();
+        if (stopFlag) {
+          return;
+        }
         var parentTr = e.currentTarget.parentNode;
         var parentTbody = e.currentTarget.parentNode.parentNode;
         var box = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
@@ -40,17 +48,26 @@ document.querySelector('#exec').addEventListener('click', function() {
             e.currentTarget.textContent = '';
           } else if (dataset[line][box] === 'X') {
             e.currentTarget.textContent = 'X';
-          } 
+          }
         }
       });
       td.addEventListener('click', function (e) {
+        if (stopFlag) {
+          return;
+        }
         var parentTr = e.currentTarget.parentNode;
         var parentTbody = e.currentTarget.parentNode.parentNode;
         var box = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
         var line = Array.prototype.indexOf.call(parentTbody.children, parentTr);
+        if (dataset[line][box] === 1) {
+          return;
+        }
         e.currentTarget.classList.add('opened');
+        openBox += 1;
         if (dataset[line][box] === 'X') {
           e.currentTarget.textContent = 'B';
+          document.querySelector('#result').textContent = 'You Lose';
+          stopFlag = true;
         } else {
           dataset[line][box] = 1;
           var around = [dataset[line][box-1], dataset[line][box+1]];
@@ -63,7 +80,7 @@ document.querySelector('#exec').addEventListener('click', function() {
           CountAroundMine = around.filter(function(v) {
             return v === 'X';
           }).length;
-          e.currentTarget.textContent = CountAroundMine;
+          e.currentTarget.textContent = CountAroundMine || '';
           if (CountAroundMine === 0) {
             var aroundBox = [];
             if (tbody.children[line-1]) {
@@ -95,6 +112,10 @@ document.querySelector('#exec').addEventListener('click', function() {
               }
             });
           }
+        }
+        if (openBox === hor * ver - mine) {
+          stopFlag = true;
+          document.querySelector('#result').textContent = 'You Win';
         }
       });
       tr.appendChild(td);
