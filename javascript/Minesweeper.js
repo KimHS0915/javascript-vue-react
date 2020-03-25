057e01d288cf727d25df13328ad0e74d014c5442
KimHS0915 <remember2552@gmail.com>
2020-03-25 23:51:38 +0900

    Update minesweeper

--- javascript/Minesweeper.js
+++ javascript/Minesweeper.js
@@ -2,6 +2,16 @@ var tbody = document.querySelector('#table tbody');
 var dataset = [];
 var stopFlag = false;
 var openBox = 0;
+var codeDict = {
+  openBox: -1,
+  question: -2,
+  flag: -3,
+  flagMine: -4,
+  questionMine: -5,
+  mine: 1,
+  normalBox: 0,
+};
+
 document.querySelector('#exec').addEventListener('click', function() {
   tbody.innerHTML = '';
   document.querySelector('#result').textContent = '';
@@ -18,7 +28,7 @@ document.querySelector('#exec').addEventListener('click', function() {
       return index;
     });
   var shuffle = [];
-  while (numArray.length >  80) {
+  while (numArray.length >  hor * ver - mine) {
     var moveValue = numArray.splice(Math.floor(Math.random() * numArray.length), 1)[0];
     shuffle.push(moveValue);
   }
@@ -28,7 +38,7 @@ document.querySelector('#exec').addEventListener('click', function() {
     var tr = document.createElement('tr');
     dataset.push(arr);
     for (var j = 0; j < hor; j++) {
-      arr.push(0); 
+      arr.push(codeDict.normalBox); 
       var td = document.createElement('td');
       td.addEventListener('contextmenu', function (e) {
         e.preventDefault();
@@ -41,13 +51,29 @@ document.querySelector('#exec').addEventListener('click', function() {
         var line = Array.prototype.indexOf.call(parentTbody.children, parentTr);
         if (e.currentTarget.textContent === '' || e.currentTarget.textContent === 'X') {
           e.currentTarget.textContent = '!';
+          e.currentTarget.classList.add('flag');
+          if (dataset[line][box] === codeDict.mine) {
+            dataset[line][box] = codeDict.flagMine;
+          } else {
+            dataset[line][box] = codeDict.flag;
+          }
         } else if (e.currentTarget.textContent === '!') {
           e.currentTarget.textContent = '?';
+          e.currentTarget.classList.remove('flag');
+          e.currentTarget.classList.add('question');
+          if (dataset[line][box] === codeDict.flagMine) {
+            dataset[line][box] = codeDict.questionMine;
+          } else {
+            dataset[line][box] = codeDict.question;
+          }
         } else if (e.currentTarget.textContent === '?') {
-          if (dataset[line][box] === 1) {
-            e.currentTarget.textContent = '';
-          } else if (dataset[line][box] === 'X') {
+          e.currentTarget.classList.remove('question');
+          if (dataset[line][box] === codeDict.questionMine) {
             e.currentTarget.textContent = 'X';
+            dataset[line][box] = codeDict.mine;
+          } else {
+            e.currentTarget.textContent = '';
+            dataset[line][box] = codeDict.normalBox;
           }
         }
       });
@@ -59,17 +85,16 @@ document.querySelector('#exec').addEventListener('click', function() {
         var parentTbody = e.currentTarget.parentNode.parentNode;
         var box = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
         var line = Array.prototype.indexOf.call(parentTbody.children, parentTr);
-        if (dataset[line][box] === 1) {
+        if ([codeDict.openBox, codeDict.flag, codeDict.flagMine, codeDict.questionMine, codeDict.question].includes(dataset[line][box])) {
           return;
         }
         e.currentTarget.classList.add('opened');
         openBox += 1;
-        if (dataset[line][box] === 'X') {
+        if (dataset[line][box] === codeDict.mine) {
           e.currentTarget.textContent = 'B';
           document.querySelector('#result').textContent = 'You Lose';
           stopFlag = true;
         } else {
-          dataset[line][box] = 1;
           var around = [dataset[line][box-1], dataset[line][box+1]];
           if (dataset[line-1]) {
             around = around.concat([dataset[line-1][box-1], dataset[line-1][box], dataset[line-1][box+1]]);
@@ -78,9 +103,10 @@ document.querySelector('#exec').addEventListener('click', function() {
             around = around.concat([dataset[line+1][box-1], dataset[line+1][box], dataset[line+1][box+1]]);
           }
           CountAroundMine = around.filter(function(v) {
-            return v === 'X';
+            return v === codeDict.mine;
           }).length;
           e.currentTarget.textContent = CountAroundMine || '';
+          dataset[line][box] = codeDict.openBox;
           if (CountAroundMine === 0) {
             var aroundBox = [];
             if (tbody.children[line-1]) {
@@ -107,7 +133,7 @@ document.querySelector('#exec').addEventListener('click', function() {
               var parentTbody = e.currentTarget.parentNode.parentNode;
               var nextBoxBox = Array.prototype.indexOf.call(parentTr.children, e.currentTarget);
               var NextBoxLine = Array.prototype.indexOf.call(parentTbody.children, parentTr);
-              if (dataset[NextBoxLine][nextBoxBox] === 1){
+              if (dataset[NextBoxLine][nextBoxBox] === codeDict.openBox){
                 nextBox.click();
               }
             });
@@ -123,9 +149,9 @@ document.querySelector('#exec').addEventListener('click', function() {
     tbody.appendChild(tr);
   }
   for (var k = 0; k < shuffle.length; k++) {
-    var col = Math.floor(shuffle[k] / 10);
-    var row = shuffle[k] % 10;
+    var col = Math.floor(shuffle[k] / ver);
+    var row = shuffle[k] % ver;
     tbody.children[col].children[row].textContent = 'X';
-    dataset[col][row] = 'X';
+    dataset[col][row] = codeDict.mine;
   }
 });
