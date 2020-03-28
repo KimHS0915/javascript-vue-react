deca283fdd1430a4374b1cf87762eca6afa483ef
KimHS0915 <remember2552@gmail.com>
2020-03-28 23:50:47 +0900

    Update tic-tac-toe
    Add CPU's turn
    Refactoring (Add function chcek, reset)

--- javascript/ticTacToe.js
+++ javascript/ticTacToe.js
@@ -5,41 +5,75 @@ var boxes = [];
 var turn = 'X';
 var result = document.createElement('div');
 
+function check(targetRow, targetBox) {
+  var complete = false;
+  if (boxes[targetRow][0].textContent === turn && boxes[targetRow][1].textContent === turn && boxes[targetRow][2].textContent === turn) {
+    complete = true;
+  }
+  if (boxes[0][targetBox].textContent === turn && boxes[1][targetBox].textContent === turn && boxes[2][targetBox].textContent === turn) {
+    complete = true;
+  }
+  if (
+    (boxes[0][0].textContent === turn && boxes[1][1].textContent === turn && boxes[2][2].textContent === turn) ||
+    (boxes[0][2].textContent === turn && boxes[1][1].textContent === turn && boxes[2][0].textContent === turn)
+  ) {
+    complete = true;
+  }
+  return complete;
+}
+
+function reset() {
+  result.textContent = turn + ' Win!';
+  turn = 'X';
+  boxes.forEach(function (row) {
+    row.forEach(function (box) {
+      box.textContent = '';
+    });
+  });
+}
+
+
 var asyncCallback = function (event) {
+  if (turn === 'O') {
+    return;
+  }
   var targetRow = rows.indexOf(event.target.parentNode);
   var targetBox = boxes[targetRow].indexOf(event.target);
 
   if (boxes[targetRow][targetBox].textContent === '') {
     boxes[targetRow][targetBox].textContent = turn;
-    var complete = false;
-    if (boxes[targetRow][0].textContent === turn && boxes[targetRow][1].textContent === turn && boxes[targetRow][2].textContent === turn) {
-      complete = true;
-    }
-    if (boxes[0][targetBox].textContent === turn && boxes[1][targetBox].textContent === turn && boxes[2][targetBox].textContent === turn) {
-      complete = true;
-    }
-    if (
-      (boxes[0][0].textContent === turn && boxes[1][1].textContent === turn && boxes[2][2].textContent === turn) ||
-      (boxes[0][2].textContent === turn && boxes[1][1].textContent === turn && boxes[2][0].textContent === turn)
-    ) {
-      complete = true;
-    }
+
+    var complete = check(targetRow, targetBox);
 
     if (complete) {
-      result.textContent = turn + ' Win!';
-      turn = 'X';
-      boxes.forEach(function (row) {
-        row.forEach(function (box) {
-          box.textContent = '';
-        })
-      })
-     
+      reset();
     } else {
       if (turn === 'X') {
         turn = 'O';
-      } else {
-        turn = 'X';
       }
+      setTimeout(function() {
+        console.log('CPU TURN');
+        var boxList = [];
+        boxes.forEach(function (row) {
+          row.forEach(function (box) {
+            boxList.push(box);
+          });
+        });
+        boxList = boxList.filter(function (box) {
+          return !box.textContent;
+        });
+        var SelectBox = boxList[Math.floor(Math.random() * boxList.length)];
+        SelectBox.textContent = 'O';
+
+        var targetRow = rows.indexOf(SelectBox.parentNode);
+        var targetBox = boxes[targetRow].indexOf(SelectBox);
+        var complete = check(targetRow, targetBox);
+
+        if (complete) {
+          reset();
+        }
+        turn = 'X';
+      }, 1000);
     }
   }
 };
