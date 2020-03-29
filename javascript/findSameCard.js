4cefb991f5e02e1327f26b01a820fd2997c34daf
KimHS0915 <remember2552@gmail.com>
2020-03-29 23:10:14 +0900

    Add card color

--- javascript/findSameCard.js
+++ javascript/findSameCard.js
@@ -1,5 +1,13 @@
 var hor = 4;
 var ver = 3;
+var colorList = ['red', 'red', 'orange', 'orange', 'yellow', 'yellow', 'green', 'green', 'blue', 'blue', 'indigo', 'indigo']
+var colors = []
+var clickFlag = false;
+
+for (var i = 0; colorList.length > 0; i++) {
+  colors = colors.concat(colorList.splice(Math.floor(Math.random() * colorList.length), 1));
+}
+console.log(colors);
 
 function cardSetting (hor, ver) {
   for (var i = 0; i < hor * ver; i++) {
@@ -11,16 +19,32 @@ function cardSetting (hor, ver) {
     cardFront.className = 'card-front';
     var cardBack = document.createElement('div');
     cardBack.className = 'card-back';
+    cardBack.style.backgroundColor = colors[i];
     cardInner.appendChild(cardFront);
     cardInner.appendChild(cardBack);
     card.appendChild(cardInner);
     (function (c) {
       card.addEventListener('click', function() {
-        c.classList.toggle('flipped');
+        if (clickFlag) {
+          c.classList.toggle('flipped');
+        }
       });
     })(card);
     document.body.appendChild(card);
   }
+
+  document.querySelectorAll('.card').forEach(function (card, index) {
+    setTimeout(function() {
+      card.classList.add('flipped');
+    }, 1000 + 100 * index);
+  });
+
+  setTimeout(function() {
+    document.querySelectorAll('.card').forEach(function (card, index) {
+      card.classList.remove('flipped');
+    });
+    clickFlag = true;
+  }, 5000);
 }
 
 cardSetting(hor, ver);
