c747e1d4bfeff2979bc8cfa96f504eebe22091ea
KimHS0915 <remember2552@gmail.com>
2020-03-29 23:34:46 +0900

    Update find same card

--- javascript/findSameCard.js
+++ javascript/findSameCard.js
@@ -1,8 +1,10 @@
 var hor = 4;
 var ver = 3;
-var colorList = ['red', 'red', 'orange', 'orange', 'yellow', 'yellow', 'green', 'green', 'blue', 'blue', 'indigo', 'indigo']
-var colors = []
+var colorList = ['red', 'red', 'orange', 'orange', 'yellow', 'yellow', 'green', 'green', 'blue', 'blue', 'indigo', 'indigo'];
+var colors = [];
 var clickFlag = false;
+var clickCards = [];
+var completeCards = [];
 
 for (var i = 0; colorList.length > 0; i++) {
   colors = colors.concat(colorList.splice(Math.floor(Math.random() * colorList.length), 1));
@@ -25,8 +27,24 @@ function cardSetting (hor, ver) {
     card.appendChild(cardInner);
     (function (c) {
       card.addEventListener('click', function() {
-        if (clickFlag) {
+        if (clickFlag && !completeCards.includes(c)) {
           c.classList.toggle('flipped');
+          clickCards.push(c);
+          if (clickCards.length === 2) {
+            if (clickCards[0].querySelector('.card-back').style.backgroundColor === clickCards[1].querySelector('.card-back').style.backgroundColor) {
+              completeCards.push(clickCards[0]);
+              completeCards.push(clickCards[1]);
+              clickCards = [];
+            } else {
+              clickFlag = false;
+              setTimeout(function() {
+                clickCards[0].classList.remove('flipped');
+                clickCards[1].classList.remove('flipped');
+                clickFlag = true;
+                clickCards = [];
+              }, 1000);
+            }
+          }
         }
       });
     })(card);
