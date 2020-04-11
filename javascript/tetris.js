2a6ad0df3a740ebe44f64ebf6bc889b3594ee513
KimHS0915 <remember2552@gmail.com>
2020-04-11 22:06:26 +0900

    Add tetris

---
+++ javascript/tetris.js
@@ -0,0 +1,17 @@
+var tetris = document.querySelector('#tetris');
+var tetrisData = [];
+
+function CreateBox () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < 20; i++) {
+    var tr = document.createElement('tr');
+    fragment.appendChild(tr);
+    for (var j = 0; j < 10; j++) {
+      var td = document.createElement('td');
+      tr.appendChild(td);
+    }
+  }
+  tetris.appendChild(fragment);
+}
+
+CreateBox();
