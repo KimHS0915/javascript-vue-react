6a099e18e1f5ad884489b594e0dd71077d8b5929
KimHS0915 <remember2552@gmail.com>
2020-04-17 23:04:09 +0900

    Add bulls-and-cows

---
+++ vue/bulls-and-cows/bulls-and-cows.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ result }}</h1>
+    <form v-on:submit="onSubmitForm">
+      <input ref="answer" maxlength="4" v-model="value" />
+      <button>input</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '',
+      result: '',
+    }
+  },
+  method: {
+    onSubmitForm(event) {
+      event.preventDefault();
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ vue/bulls-and-cows/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import bullsAndCows from './bulls-and-cows';
+
+new Vue(bullsAndCows).$mount('#root');
