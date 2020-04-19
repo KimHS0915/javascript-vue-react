073f3785cb42ca9c3ea8caa4766a400e1e762309
KimHS0915 <remember2552@gmail.com>
2020-04-19 22:42:43 +0900

    Update reaction-time-test

--- vue/reaction-time-test/reaction-time-test.vue
+++ vue/reaction-time-test/reaction-time-test.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <div id="screen" :class="state" @click="onClickScreen">{{ message }}</div>
-    <div>
-      <div>Average Time : </div>
+    <div v-show="result.length">
+      <div>{{ result[result.length-1] }}ms</div>
+      <div>Average Time : {{ average }}ms</div>
       <button @click="onReset">Reset</button>
     </div>
   </div>
 </template>
 
 <script>
+let startTime = 0;
+let endTime = 0;
+let timeOut = null;
 export default {
   data() {
     return {
@@ -17,17 +21,33 @@ export default {
       message: 'Click anywhere to start',
     };
   },
+  computed: {
+    average() {
+      return this.result.reduce((a, c) => a + c, 0) / this.result.length;
+    }
+  },
   methods: {
     onReset() {
-
+      this.result = [];
     },
     onClickScreen() {
       if (this.state === 'waiting') {
         this.state = 'ready';
+        this.message = 'Wait for green';
+        timeOut = setTimeout(() => {
+          this.state = 'now';
+          this.message = 'Click!';
+          startTime = new Date();
+        }, Math.floor(Math.random() * 1000) + 2000)
       } else if (this.state === 'ready') {
-        this.state = 'now';        
-      } else if (this.state === 'now') {
+        clearTimeout(timeOut);
         this.state = 'waiting';
+        this.message = 'Too soon! Click to try again';        
+      } else if (this.state === 'now') {
+        endTime = new Date();
+        this.state = 'waiting';
+        this.message = 'Click anywhere to start';
+        this.result.push(endTime - startTime);
       }
     },
   },
