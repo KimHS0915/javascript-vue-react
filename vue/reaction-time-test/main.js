aebee43b8a6e405e686464a3dbab678cdbb7cf47
KimHS0915 <remember2552@gmail.com>
2020-04-19 20:13:59 +0900

    Add reaction-time-test

---
+++ vue/reaction-time-test/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import ReactionTimeTest from './reaction-time-test';
+
+new Vue(ReactionTimeTest).$mount('#root');
---
+++ vue/reaction-time-test/reaction-time-test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div id="screen">{{ message }}</div>
+    <div>
+      <div>Average Time : </div>
+      <button @click="onReset">Reset</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      result: [],
+      state: 'waiting',
+      message: 'Click anywhere to start',
+    }
+  },
+  method: {
+    onReset() {}
+  }
+};
+</script>
+
+<style>
+
+</style>
