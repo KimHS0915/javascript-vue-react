52d4a0d6275fad70507219293a94d0f6a21f85ff
KimHS0915 <remember2552@gmail.com>
2020-04-20 23:13:15 +0900

    Add rock-paper-scissors

---
+++ vue/rock-paper-scissors/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import rockPaperScissors from './rock-paper-scissors';
+
+new Vue(rockPaperScissors).$mount('#root');
---
+++ vue/rock-paper-scissors/rock-paper-scissors.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div id="computer"></div>
+    <div>
+      <button>Rock</button>
+      <button>Paper</button>
+      <button>Scissors</button>
+    </div>
+    <div>{{ result }}</div>
+    <div>Score : {{ score }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      result: '',
+      score: 0,
+    };
+  },
+  computed: {
+
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
