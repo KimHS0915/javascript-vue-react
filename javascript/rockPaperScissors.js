6672a9808431faffb2fc068fb7b642880f3aefb9
KimHS0915 <remember2552@gmail.com>
2020-04-21 22:58:02 +0900

    Update rock-paper-scissors
    Fix typo
    Change variable name

--- javascript/rockPaperScissors.js
+++ javascript/rockPaperScissors.js
@@ -1,28 +1,28 @@
-var imgLocation = 0;
-var rockPaperScissors = {
+var imgCoord = 0;
+var coords = {
   rock: '0',
   paper: '-284px',
   scissors: '-142px'
 };
 
-function comChoice (imgLocation) {
-  return Object.entries(rockPaperScissors).find(function (v) {
-    return v[1] === imgLocation;
+function comChoice (imgCoord) {
+  return Object.entries(coords).find(function (v) {
+    return v[1] === imgCoord;
   })[0];
 }
 
 var interval;
 function intervalMaker() {
   interval = setInterval(function () {
-    if (imgLocation === rockPaperScissors.rock) {
-      imgLocation = rockPaperScissors.paper;
-    } else if (imgLocation === rockPaperScissors.paper) {
-      imgLocation = rockPaperScissors.scissors;
+    if (imgCoord === coords.rock) {
+      imgCoord = coords.paper;
+    } else if (imgCoord === coords.paper) {
+      imgCoord = coords.scissors;
     } else {
-      imgLocation = rockPaperScissors.rock;
+      imgCoord = coords.rock;
     }
     document.querySelector('#computer').style.background =
-      'url(img/rockPaperScissors.jpg) ' + imgLocation + ' 0';
+      'url(img/rockPaperScissors.jpg) ' + imgCoord + ' 0';
   }, 100); 
 } 
 
@@ -52,16 +52,16 @@ document.querySelectorAll('.btn').forEach(function (btn) {
     }, 1000);
       
     var myChoice = (this.textContent).toLowerCase();
-    var score = scoreboard[myChoice] - scoreboard[comChoice(imgLocation)];
+    var score = scoreboard[myChoice] - scoreboard[comChoice(imgCoord)];
     if (score == 0) {
-      message.textContent = 'Draw'
-      draw += 1
+      message.textContent = 'Draw';
+      draw += 1;
     } else if ([-2, 1].includes(score)) { // (score == -2 || score == 1)
-      message.textContent = 'You Win'
-      win += 1
+      message.textContent = 'You Win';
+      win += 1;
     } else {
-      message.textContent = 'You Lose'
-      lose += 1
+      message.textContent = 'You Lose';
+      lose += 1;
     }
     record.textContent = `Win : ${win}, Draw : ${draw}, Lose : ${lose}`;
   });
