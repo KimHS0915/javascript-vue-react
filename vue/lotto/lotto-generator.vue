47599d3263fb05bdd44e6a763f74292e37596306
KimHS0915 <remember2552@gmail.com>
2020-04-22 23:01:20 +0900

    Add lotto

---
+++ vue/lotto/lotto-generator.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div>Winning Number</div>
+    <div id="result">
+      <lotto-ball v-for="ball in winBalls" :key="ball"></lotto-ball>
+    </div>
+    <div>Bonus</div>
+    <lotto-ball v-if="bonus"></lotto-ball>
+    <button v-if="reset">Reset</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      winBalls: [],
+      bonus: null,
+      reset: false,
+    };
+  },
+  computed: {
+  
+  },
+  methods: {
+
+  },
+  mounted() {
+
+  },
+  beforeDestroy() {
+
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/lotto/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import lottoGenerator from './lotto-generator';
+
+new Vue(lottoGenerator).$mount('#root');
