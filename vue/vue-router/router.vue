c869a824ed4f206d37eb19086f45f11ddff3f252
KimHS0915 <remember2552@gmail.com>
2020-05-04 23:31:19 +0900

    Update Navbar

--- vue/vue-router/router.vue
+++ vue/vue-router/router.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
   <div>
-    <nav class="navbar navbar-expand-lg navbar-white bg-white">
-      <a class="navbar-brand" href="#"><router-link to="/game/index">Index</router-link></a>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#"><router-link to="/game/index" class="index">Index</router-link></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -10,22 +10,22 @@
         <ul class="navbar-nav mr-auto">
           <li class="nav-item mx-3">
             <a class="nav-link" href="#">
-              <router-link to="/game/bulls-and-cows">Bulls And Cows</router-link>
+              <router-link to="/game/bulls-and-cows" class="router-link">Bulls And Cows</router-link>
             </a>
           </li>
           <li class="nav-item mx-3">
             <a class="nav-link" href="#">
-              <router-link to="/game/reaction-time-test">Reaction Time Test</router-link>
+              <router-link to="/game/reaction-time-test" class="router-link">Reaction Time Test</router-link>
             </a>
           </li>
           <li class="nav-item mx-3">
             <a class="nav-link" href="#">
-              <router-link to="/game/lotto-generator">Lotto Generator</router-link>
+              <router-link to="/game/lotto-generator" class="router-link">Lotto Generator</router-link>
             </a>
           </li>
           <li class="nav-item mx-3">
             <a class="nav-link" href="#">
-              <router-link to="/game/rock-paper-scissors">Rock Paper Scissors</router-link>
+              <router-link to="/game/rock-paper-scissors" class="router-link">Rock Paper Scissors</router-link>
             </a>
           </li>
         </ul>
@@ -56,6 +56,16 @@ td {
   height: 50px;
   text-align: center;
   font-size: 30px;
+}
+.index {
+  font-size: 30px;
+  font-weight: bold;
+  color: white;
+}
+.router-link {
+  font-size: 20px;
+  font-style: italic;
+  color: white;
 }
 #computer {
   width: 142px;
