6992e579099302b70a18ef4fde729fd9be13bee3
KimHS0915 <remember2552@gmail.com>
2020-05-04 23:24:04 +0900

    Add Navbar

--- vue/vue-router/router.vue
+++ vue/vue-router/router.vue
@@ -1,13 +1,40 @@
 <template>
 <div>
   <div>
-    <router-link to="/game/index">Index</router-link>
-    <router-link to="/game/bulls-and-cows">Bulls And Cows</router-link>
-    <router-link to="/game/reaction-time-test">Reaction Time Test</router-link>
-    <router-link to="/game/lotto-generator">Lotto Generator</router-link>
-    <router-link to="/game/rock-paper-scissors">Rock Paper Scissors</router-link>
+    <nav class="navbar navbar-expand-lg navbar-white bg-white">
+      <a class="navbar-brand" href="#"><router-link to="/game/index">Index</router-link></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item mx-3">
+            <a class="nav-link" href="#">
+              <router-link to="/game/bulls-and-cows">Bulls And Cows</router-link>
+            </a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link" href="#">
+              <router-link to="/game/reaction-time-test">Reaction Time Test</router-link>
+            </a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link" href="#">
+              <router-link to="/game/lotto-generator">Lotto Generator</router-link>
+            </a>
+          </li>
+          <li class="nav-item mx-3">
+            <a class="nav-link" href="#">
+              <router-link to="/game/rock-paper-scissors">Rock Paper Scissors</router-link>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
-  <router-view></router-view>
+  <p>
+    <router-view></router-view>
+  </p>
 </div>
 </template>
 
