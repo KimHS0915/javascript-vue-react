25907ad54f9ee075bd240c34e9320ae1697f2940
KimHS0915 <remember2552@gmail.com>
2020-05-16 16:30:16 +0900

    Update ReactionTimeTestClass

--- react/ReactionTimeTest/ReactionTimeTestClass.jsx
+++ react/ReactionTimeTest/ReactionTimeTestClass.jsx
@@ -8,19 +8,66 @@ class ReactionTimeTest extends PureComponent {
     message: 'Click anywhere to start',
   };
 
-  onClickScreen = () => {};
+  timeOut;
+  startTime;
+  endTime;
+
+  onClickScreen = () => {
+    const { result, state, message } = this.state;
+    if (state === 'waiting') {
+      this.setState({
+        state: 'ready',
+        message: 'Wait for green',
+      });
+      this.timeOut = setTimeout(() => {
+        this.setState({
+          state: 'now',
+          message: 'Click!',
+        });
+        this.startTime = new Date();
+      }, Math.floor(Math.random() * 1000) + 2000);
+    } else if (state === 'ready') {
+      clearTimeout(this.timeOut);
+      this.setState({
+        state: 'waiting',
+        message: 'Too soon! Click to try again',
+      });
+    } else if (state === 'now') {
+      this.endTime = new Date();
+      this.setState((prevState) => {
+        return {
+          result: [...prevState.result, this.endTime - this.startTime],
+          state: 'waiting',
+          message: 'Click anywhere to start',
+        };
+      });
+    }
+  };
+
+  renderAverage = () => {
+    const { result } = this.state;
+    return (
+      result.length === 0 ? null :
+      <div>Average Time : {result.reduce((a, c) => a + c) / result.length}ms</div>
+    );
+  };
+
+  onReset = () => {
+    const { result } = this.state;
+    this.setState({
+      result: [],
+    });
+  };
 
   render() {
+    const { state, message } = this.state;
     return (
       <>
-        <div id="screen" className={this.state.state} onClick={this.onClickScreen}>
-          {this.state.message}
+        <div id="screen" className={state} onClick={this.onClickScreen}>
+          {message}
         </div>
-        {
-        this.state.result.length === 0 ? null :
-        <div>Average Time : {this.state.result.reduce((a, c) => a + c) / this.state.result.length}ms</div>
-        }
-        <button>Reset</button>
+        <button onClick={this.onReset}>Reset</button>
+        {this.renderAverage()}
       </>
     );
   }
