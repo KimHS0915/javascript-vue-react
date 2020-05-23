fe9399b9c61da438de5825aabf5cad8058f1b5e9
KimHS0915 <remember2552@gmail.com>
2020-05-24 00:31:37 +0900

    Provider, createContext

--- react/MineSweeper/Form.jsx
+++ react/MineSweeper/Form.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useContext } from 'react';
+import { START_GAME, TableContext } from './MineSweeper';
 
 const Form = () => {
   const [row, setRow] = useState(10);
   const [cell, setCell] = useState(10);
   const [mine, setMine] = useState(20);
+  const { dispatch } = useContext(TableContext);
 
   const onChangeRow = useCallback((e) => {
     setRow(e.target.value);
@@ -17,7 +19,9 @@ const Form = () => {
     setMine(e.target.value);
   }, []);
 
-  const onClickButton = useCallback((e) => {}, []);
+  const onClickButton = useCallback((e) => {
+    dispatch({ type: START_GAME, row, cell, mine });    
+  }, [row, cell, mine]);
 
   return (
     <div>
--- react/MineSweeper/MineSweeper.jsx
+++ react/MineSweeper/MineSweeper.jsx
@@ -1,6 +1,13 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, createContext, useMemo } from 'react';
 import Table from './Table';
 import Form from './Form'
+
+export const CODE = {};
+
+export const TableContext = createContext({
+  tableData: [],
+  dispatch: () => {},
+});
 
 const initialState = {
   tableData: [],
@@ -8,8 +15,17 @@ const initialState = {
   result: '',
 };
 
+const plantMine = () => {};
+
+export const START_GAME = 'START_GAME';
+
 const reducer = (state, action) => {
   switch (action.type) {
+    case START_GAME:
+      return {
+        ...state,
+        tableData: plantMine(action.row, action.cell, action.mine),
+      };
     default:
       return state;
   }
@@ -18,13 +34,15 @@ const reducer = (state, action) => {
 const MineSweeper = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const value = useMemo(() => ({ tableData: state.tableData, dispatch}), [state.tableData]);
+
   return (
-    <>
+    <TableContext.Provider value={value}>
       <Form />
       <div>{state.timer}</div>
       <Table />
       <div>{state.result}</div>
-    </>
+    </TableContext.Provider>
   );
 };
 
