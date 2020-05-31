31d8a05bfcfe21337ee44ab619fcf529cf6e30df
KimHS0915 <remember2552@gmail.com>
2020-05-31 14:40:56 +0900

    Update Router
    Add Lotto

--- react/ReactRouter/Router.jsx
+++ react/ReactRouter/Router.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import BullsAndCows from '../BullsAndCows/BullsAndCowsClass';
+import Lotto from '../Lotto/LottoClass';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <div>
         <Link to="/BullsAndCows">Bulls And Cows</Link>
+        &nbsp;
+        <Link to="/Lotto">Lotto</Link>
       </div>
       <div>
         <Route path="/BullsAndCows" component={BullsAndCows} />
+        <Route path="/Lotto" component={Lotto} />
       </div>
     </BrowserRouter>
   );
